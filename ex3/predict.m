c3fd734cadf312abe531a41d5a2745e37ec05b19
zhengjie9510 <zhengjie9510@qq.com>
2022-03-03 17:28:57 +0800

    complete ex3

--- ex3/predict.m
+++ ex3/predict.m
@@ -21,7 +21,11 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
-
+z2 = [ones(size(X,1), 1), X] * Theta1';
+a2 = sigmoid(z2);
+z3 = [ones(size(a2,1), 1), a2] * Theta2';
+a3 = sigmoid(z3);
+[~, p] = max(a3, [], 2);
 
 
 
